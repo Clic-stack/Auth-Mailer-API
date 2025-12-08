249312f9f19c500c7f80c0c386802e3c94ea80d7
Clic-stack <175146257+Clic-stack@users.noreply.github.com>
2025-12-08 14:11:47 -0600

    Fourth commit: fix mailer config in production

--- email-api/src/mails/mailer.js
+++ email-api/src/mails/mailer.js
@@ -2,7 +2,10 @@ import nodemailer from 'nodemailer'
 import { env } from '../config/env.js'
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    //service: "gmail",
     auth: {
         user: env.EMAIL,
         pass: env.GOOGLE_APP_PASSWORD
@@ -11,5 +14,11 @@ const transporter = nodemailer.createTransport({
 
 export const sendEmail = async (options) => {
     const mailOptions = { from: env.EMAIL, ...options }
+    try {
     await transporter.sendMail(mailOptions)
+    console.log("Email sent:", options.to)
+  } catch (err) {
+    console.error("Error sending email:", err)
+    throw err
+  }
 }
