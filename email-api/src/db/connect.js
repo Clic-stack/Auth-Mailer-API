a90d72482658a943f4b45b9ca8bf5e3383f3611e
Clic-stack <175146257+Clic-stack@users.noreply.github.com>
2025-12-08 14:20:17 -0600

    Fifth commit: Delete trashcode

--- email-api/src/db/connect.js
+++ email-api/src/db/connect.js
@@ -1,8 +1,6 @@
 import { Sequelize } from 'sequelize'
 import { env } from '../config/env.js'
 
-const { Sequelize } = require("sequelize")
-
 const sequelize = new Sequelize(env.DATABASE_URL, {
   dialect: 'postgres',
   logging: false,
