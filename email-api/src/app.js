c7f85656cc37e482d4798a8935fb8c67dfa02a33
Clic-stack <175146257+Clic-stack@users.noreply.github.com>
2025-12-09 15:04:40 -0600

    Delete debugger email from app.js

--- email-api/src/app.js
+++ email-api/src/app.js
@@ -5,7 +5,6 @@ import morgan from 'morgan'
 import routes from './routes/index.js'
 import { errorHandler } from './middlewares/errorHandler.js'
 import { env } from './config/env.js'
-import { sendEmail } from './mails/mailer.js'
 
 const app = express()
 
@@ -16,20 +15,6 @@ app.use(helmet())
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-
-// Debug SMTP (temporal)
-app.get('/debug/send-mail', async (req, res) => {
-  try {
-    await sendEmail({
-      to: env.EMAIL, // o tu correo de prueba
-      subject: 'Test desde Render (SSL 465)',
-      html: '<p>Si ves este correo, SMTP funciona en producción</p>'
-    })
-    res.json({ message: 'Correo enviado' })
-  } catch (err) {
-    res.status(500).json({ message: 'Error enviando correo', error: err.message })
-  }
-})
 
 // Routes
 app.use('/', routes)
