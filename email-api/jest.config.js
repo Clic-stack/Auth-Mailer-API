6a4545009dbb34bd6dc96b8e52632105cb5c10c8
Clic-stack <175146257+Clic-stack@users.noreply.github.com>
2026-04-01 19:48:39 -0600

    fix: remove redundant ESM option and expand transform patterns for jsdom dependencies

--- email-api/jest.config.js
+++ email-api/jest.config.js
@@ -2,13 +2,11 @@ import 'dotenv/config';
 
 export default {
   testEnvironment: 'node',
-  // ESTA LÍNEA ES LA CLAVE:
-  extensionsToTreatAsEsm: ['.js'], 
   transform: {
     "^.+\\.(t|j)sx?$": "@swc/jest",
   },
   transformIgnorePatterns: [
-    "/node_modules/(?!(dompurify|jsdom|@exodus/bytes|html-encoding-sniffer|encoding-lite)/)"
+    "/node_modules/(?!(dompurify|jsdom|@exodus/bytes|html-encoding-sniffer|encoding-lite|abab|tr46|whatwg-url|data-urls|decimal.js)/)"
   ],
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
