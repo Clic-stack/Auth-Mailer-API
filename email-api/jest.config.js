6af6f9791c421d6269b32fa1ac2a5ce61f28feee
Clic-stack <175146257+Clic-stack@users.noreply.github.com>
2026-04-02 13:31:51 -0600

    docs: change jest configuration

--- email-api/jest.config.js
+++ email-api/jest.config.js
@@ -1,10 +1,31 @@
-import 'dotenv/config';
+/*import 'dotenv/config';
 
 export default {
   testEnvironment: 'node',
   transform: {
     "^.+\\.(t|j)sx?$": "@swc/jest",
   },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+};*/
+
+import 'dotenv/config';
+
+export default {
+  testEnvironment: 'node',
+  transform: {
+    "^.+\\.jsx?$": ["@swc/jest", {
+      jsc: {
+        parser: {
+          syntax: "ecmascript",
+          jsx: true,
+        },
+        transform: {},
+      },
+    }],
+  },
+  extensionsToTreatAsEsm: ['.js'],
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
   },
