6c4ca819c111838307add7435c4396ca442c121f
Clic-stack <175146257+Clic-stack@users.noreply.github.com>
2026-04-01 20:44:06 -0600

    docs: update jest.config.js and test script inpackage.json

--- email-api/jest.config.js
+++ email-api/jest.config.js
@@ -5,8 +5,11 @@ export default {
   transform: {
     "^.+\\.(t|j)sx?$": "@swc/jest",
   },
+  // Añadimos esta línea para ayudar a Jest a encontrar los módulos
+  moduleDirectories: ['node_modules', 'src'],
   transformIgnorePatterns: [
-    "/node_modules/(?!(dompurify|jsdom|@exodus/bytes|html-encoding-sniffer|encoding-lite|abab|tr46|whatwg-url|data-urls|decimal.js)/)"
+    // Esta expresión regular le dice a Jest: "Transforma todo lo que esté en node_modules EXCEPTO estas librerías"
+    "/node_modules/(?!(dompurify|jsdom|@exodus/bytes|html-encoding-sniffer|encoding-lite|abab|tr46|whatwg-url|data-urls|decimal.js|saxes)/)"
   ],
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
