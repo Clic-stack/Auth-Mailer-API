055255a18284bad503ad0ac93be2d1d615fa050f
Clic-stack <175146257+Clic-stack@users.noreply.github.com>
2026-04-01 19:28:49 -0600

    fix: resolve multiple jest configurations conflict and centralize in jest.config.js

--- email-api/jest.config.js
+++ email-api/jest.config.js
@@ -3,8 +3,12 @@ import 'dotenv/config'; // Esto cargará el archivo .env de la raíz
 export default {
   testEnvironment: 'node',
   transform: {
-    '^.+\\.(t|j)sx?$': '@swc/jest',
+    "^.+\\.(t|j)sx?$": "@swc/jest",
   },
+  transformIgnorePatterns: [
+    "/node_modules/(?!(dompurify|jsdom|@exodus/bytes)/)"
+  ],
+  
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
   },
