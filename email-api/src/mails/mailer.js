65d7510de934977307c7775cdda278f56fc912ec
Clic-stack <175146257+Clic-stack@users.noreply.github.com>
2026-04-05 15:33:58 -0600

    fix: change transporter configuration from mailer.js

--- email-api/src/mails/mailer.js
+++ email-api/src/mails/mailer.js
@@ -3,11 +3,11 @@ import { env } from '../config/env.js'
 import DOMPurify from 'isomorphic-dompurify'; // Esta librería ya trae su propio DOM interno
 
 const transporter = nodemailer.createTransport({
-  pool: true, // Reutiliza la conexión (más eficiente en Render)
+  //pool: true, // Reutiliza la conexión (más eficiente en Render)
   host: env.SMTP_HOST, 
   port: parseInt(env.SMTP_PORT),    
   secure: false, // true para puerto 465, false para otros
-  family: 4,
+  //family: 4,
   auth: {
     user: env.EMAIL,
     pass: env.GOOGLE_APP_PASSWORD,
@@ -15,11 +15,11 @@ const transporter = nodemailer.createTransport({
   tls: {
     rejectUnauthorized: false, // Esto ayuda a evitar bloqueos de certificados en Render
     servername: env.SMTP_HOST,
-    minVersion: 'TLSv1.2'
+    //minVersion: 'TLSv1.2'
   },
-  connectionTimeout: 40000, // Aumentamos a 40 segundos
-  greetingTimeout: 30000,   // 30 segundos para el saludo
-  socketTimeout: 60000     // 60 segundos de socket abierto
+  connectionTimeout: 20000, // Aumentamos a 40 segundos
+  //greetingTimeout: 30000,   // 30 segundos para el saludo
+  //socketTimeout: 60000     // 60 segundos de socket abierto
 });
 
 export const sendEmail = async ({ to, subject, html }) => {
